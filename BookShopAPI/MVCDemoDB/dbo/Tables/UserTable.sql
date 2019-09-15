12dc7c5529e0067c588fd3e47819f4118660aac2
Apepke <39323383+Apepke@users.noreply.github.com>
2019-09-15 12:42:51 -0500

    First commit

--- BookShopAPI/MVCDemoDB/dbo/Tables/UserTable.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[UserTable]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [name] NVARCHAR(50) NOT NULL, 
-    [title] NVARCHAR(50) NOT NULL, 
-    [author] NVARCHAR(50) NOT NULL, 
-    [review] NVARCHAR(200) NOT NULL
-)
